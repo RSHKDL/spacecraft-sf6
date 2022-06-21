03d14316b7b1dc3c32ca06bb5cc7c6207f609b4b
jhelfer <jhelfer@clever-age.com>
2022-06-21 15:31:55 +0200

    stimulus basic features

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+    static targets = ["amount", "emote"];
+
     connect() {
-        this.element.innerHTML = 'You have clicked me 0 times ?';
+        // controller property
+        this.count = 0
+
+        // basic eventListener : element is the html node where our controller is declared
+        this.element.addEventListener('click', () => {
+            this.count++
+            // this amountTarget is possible through static targets
+            this.amountTarget.innerText = this.count
+            // same for emoteTarget
+            if (this.count > 0) {
+                this.emoteTarget.innerText = "😀"
+            }
+            if (this.count >= 5) {
+                this.emoteTarget.innerText = "😮"
+            }
+        })
+
     }
 }

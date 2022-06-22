bb50b9164c347401a0eba45d7dd50c8abdc55273
jhelfer <jhelfer@clever-age.com>
2022-06-22 14:04:39 +0200

    new color box controller

---
+++ assets/controllers/color-box_controller.js
@@ -0,0 +1,41 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+    selectedColorId = null
+
+    static targets = ['colorSquare', 'select']
+
+    connect() {
+        this.selectTarget.classList.add('d-none')
+    }
+
+    selectColor(event) {
+        this.setSelectedColor(event.currentTarget.dataset.colorId)
+    }
+
+    setSelectedColor(clickedColorId) {
+        if (clickedColorId === this.selectedColorId) {
+            this.findSelectedColorSquare().classList.remove('selected')
+            this.selectedColorId = null
+            this.selectTarget.value = ''
+
+            return
+        }
+
+        this.selectedColorId = clickedColorId
+
+        this.colorSquareTargets.forEach((element) => {
+            element.classList.remove('selected')
+        })
+
+        this.findSelectedColorSquare().classList.add('selected')
+        this.selectTarget.value = this.selectedColorId
+    }
+
+    /**
+     * @returns {Element|null}
+     */
+    findSelectedColorSquare() {
+        return this.colorSquareTargets.find((element) => element.dataset.colorId === this.selectedColorId)
+    }
+}

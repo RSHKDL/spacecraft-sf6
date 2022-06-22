73912d8c4004824c48df3206c912aab2dacd6541
jhelfer <jhelfer@clever-age.com>
2022-06-22 14:53:44 +0200

    refacto with api values

--- assets/controllers/color-box_controller.js
+++ assets/controllers/color-box_controller.js
@@ -1,41 +1,32 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-    selectedColorId = null
-
     static targets = ['colorSquare', 'select']
+
+    // The values Api
+    static values = {
+        colorId: Number // allow types conversion because everything from dataset is a string
+    }
 
     connect() {
         this.selectTarget.classList.add('d-none')
     }
 
     selectColor(event) {
-        this.setSelectedColor(event.currentTarget.dataset.colorId)
+        const clickedColor = event.currentTarget.dataset.colorId
+        this.colorIdValue = clickedColor == this.colorIdValue ? null : clickedColor
     }
 
-    setSelectedColor(clickedColorId) {
-        if (clickedColorId === this.selectedColorId) {
-            this.findSelectedColorSquare().classList.remove('selected')
-            this.selectedColorId = null
-            this.selectTarget.value = ''
-
-            return
-        }
-
-        this.selectedColorId = clickedColorId
+    // callback function, naming is important
+    colorIdValueChanged() {
+        this.selectTarget.value = this.colorIdValue
 
         this.colorSquareTargets.forEach((element) => {
-            element.classList.remove('selected')
+            if (element.dataset.colorId == this.colorIdValue) {
+                element.classList.add('selected')
+            } else {
+                element.classList.remove('selected')
+            }
         })
-
-        this.findSelectedColorSquare().classList.add('selected')
-        this.selectTarget.value = this.selectedColorId
-    }
-
-    /**
-     * @returns {Element|null}
-     */
-    findSelectedColorSquare() {
-        return this.colorSquareTargets.find((element) => element.dataset.colorId === this.selectedColorId)
     }
 }

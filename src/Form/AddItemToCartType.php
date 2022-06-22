3d87df3d43fbfa8e3175789a3094f9049c958a89
jhelfer <jhelfer@clever-age.com>
2022-06-22 13:15:37 +0200

    add symfony form and setup basic cartItem

---
+++ src/Form/AddItemToCartType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CartItem;
+use App\Entity\Product;
+use App\Entity\ProductOptions;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddItemToCartType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $product = $options['product'];
+        $productOptions = $options['productOptions'];
+
+        $builder->setData(new CartItem($product));
+        $builder
+            ->add('color', EntityType::class, [
+                'class' => ProductOptions::class,
+                'choices' => $productOptions,
+                'placeholder' => 'Color',
+                'choice_label' => 'value',
+            ])
+            ->add('quantity', IntegerType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setRequired(['product', 'productOptions']);
+        $resolver->addAllowedTypes('product', Product::class);
+        $resolver->addAllowedTypes('productOptions', 'array');
+        $resolver->setDefaults([
+            'data_class' => CartItem::class,
+        ]);
+    }
+}

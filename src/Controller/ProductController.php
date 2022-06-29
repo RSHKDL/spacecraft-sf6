b6d7a4735f03c66e7fa4487a959bed6e041031fa
jhelfer <jhelfer@clever-age.com>
2022-06-29 13:52:08 +0200

    Adapt AddItemToCartType to previous refacto

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\CartItem;
 use App\Entity\Product;
+use App\Form\AddItemToCartType;
 use App\Repository\ProductOptionsRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,11 +30,31 @@ class ProductController extends AbstractController
     }
 
     #[Route('shop/{id}', name: 'app_product_show')]
-    public function show(Product $product): Response
+    public function show(Product $product, Request $request): Response
     {
+        $optionTypes = $this->productRepository->getDistinctOptionTypeByProduct($product);
+
+        $form = $this->createForm(AddItemToCartType::class, null, [
+            'product' => $product,
+            'optionsTypes' => $optionTypes
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            /** @var CartItem $cartItem */
+            $cartItem = $form->getData();
+            $options = [];
+            foreach ($optionTypes as $type) {
+                $options[] = $form->get("option_{$type}")->getData();
+            }
+            $cartItem->setOptions($options);
+            dd($cartItem);
+        }
+
         return $this->render(
             'product/show.html.twig', [
                 'product' => $product,
+                'form' => $form->createView()
             ]
         );
     }
--- src/Entity/CartItem.php
+++ src/Entity/CartItem.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use App\Repository\CartItemRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-//#[ORM\Entity(repositoryClass: CartItemRepository::class)]
+#[ORM\Entity(repositoryClass: CartItemRepository::class)]
 class CartItem
 {
     #[ORM\Id]
